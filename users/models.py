b0336e22c70e91a2636ea31315b1c8dd8089ae79
SUNR153 <zariptursunov3@gmail.com>
2025-03-31 15:31:11 +0500

    Second commit

--- users/models.py
+++ users/models.py
@@ -10,5 +10,11 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICER, default='renter')
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.CharField(max_length=20)
+    driver_license = models.CharField(max_length=20, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
     def __str__(self):
         return f"{self.username} ({self.role})"
